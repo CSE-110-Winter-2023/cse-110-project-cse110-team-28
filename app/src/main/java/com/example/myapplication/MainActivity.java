2f188ad76298e9b1fec6506f27e877bbf136f99c
zfxd <zhang_weiyao@yahoo.com.sg>
2023-02-11 15:23:48 -0800

    LocationService and OrientationService implemented

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,14 +1,49 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private OrientationService orientationService;
+    private LocationService locationService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED &&
+            ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    200);
+        }
+
+
+        orientationService = OrientationService.singleton(this);
+        TextView orientation_text = findViewById(R.id.orientation_text);
+        orientationService.getOrientation().observe(this, orientation -> {
+            // Update textview with latest value
+            orientation_text.setText(Float.toString(orientation));
+        });
+
+        locationService = LocationService.singleton(this);
+        TextView location_text = findViewById(R.id.location_text);
+        locationService.getLocation().observe(this, loc -> {
+            location_text.setText(Double.toString(loc.first) + " , " + Double.toString(loc.second));
+        });
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        orientationService.unregisterSensorListeners();
     }
 }
---
+++ app/src/main/java/com/example/myapplication/OrientationService.java
@@ -0,0 +1,101 @@
+package com.example.myapplication;
+
+import android.app.Activity;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+
+public class OrientationService implements SensorEventListener {
+    private static OrientationService instance;
+
+    private final SensorManager sensorManager;
+    private float[] accelerometerReading;
+    private float[] magnetometerReading;
+    private MutableLiveData<Float> azimuth;
+
+    /**
+     * Constructor for OrientationService
+     * @param activity Context needed to initiate SensorManager
+     */
+    protected OrientationService(Activity activity){
+        this.azimuth = new MutableLiveData<>();
+        this.sensorManager = (SensorManager) activity.getSystemService(Context.SENSOR_SERVICE);
+        // Register sensor listeners
+        this.registerSensorListeners();
+    }
+
+    private void registerSensorListeners() {
+        // Register our listener to the accelerometer and magnetometer.
+        // We need both pieces of data to compute the orientation!
+        sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+                SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD),
+                SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    public static OrientationService singleton(Activity activity) {
+        if (instance == null) {
+            instance = new OrientationService(activity);
+        }
+        return instance;
+    }
+
+    /**
+     * This method is called when the sensor detects a change in value.
+     * @param event the event containing the values we need.
+     */
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            accelerometerReading = event.values;
+        }
+        if (event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) {
+            magnetometerReading = event.values;
+        }
+
+        if (accelerometerReading != null && magnetometerReading != null) {
+            // We have both sensors, so we can compute the orientation
+            onBothSensorDataAvailable();
+        }
+    }
+
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        // Do nothing
+        return;
+    }
+
+    private void onBothSensorDataAvailable() {
+        // Discount contract checking
+        if (accelerometerReading == null || magnetometerReading == null) {
+            throw new IllegalStateException("Both sensors must be available to compute orientation");
+        }
+
+        var r = new float[9];
+        var i = new float[9];
+        // Linear algebra magic
+        var success = SensorManager.getRotationMatrix(r, i, accelerometerReading, magnetometerReading);
+        // Did it work?
+        if (success) {
+            var orientation = new float[3];
+            SensorManager.getOrientation(r, orientation);
+
+            // Orientation in order: azimuth, pitch, roll. We only care about azimuth
+            // (Around the z-axis from -pi to pi)
+            // 0 means north, pi means south. pi/2 is east, 3pi/2 is west.
+            this.azimuth.postValue(orientation[0]);
+        }
+    }
+
+    public void unregisterSensorListeners() { sensorManager.unregisterListener(this); }
+
+    public LiveData<Float> getOrientation() { return this.azimuth; }
+
+    public void setMockOrientationSource(MutableLiveData<Float> mockDataSource) {
+        unregisterSensorListeners();
+        this.azimuth = mockDataSource;
+    }
+}
