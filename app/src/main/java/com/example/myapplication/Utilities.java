db040486149b79fc03ddba75c22d45be6a754043
EShults2124 <shults.eric01@gmail.com>
2023-02-12 21:04:43 -0800

    Added Direction Text Display

--- app/src/main/java/com/example/myapplication/Utilities.java
+++ app/src/main/java/com/example/myapplication/Utilities.java
@@ -19,4 +19,46 @@ public class Utilities {
         alertDialog.show();
     }
 
+    public static String cardDirection(float orientation) {
+
+        if (orientation >= 0) {
+            if (orientation > 337.5 || orientation <= 22.5)
+                return "N";
+            if (orientation <= 67.5)
+                return "NE";
+            if (orientation <= 112.5)
+                return "E";
+            if (orientation <= 157.5)
+                return "SE";
+            if (orientation <= 202.5)
+                return "S";
+            if (orientation <= 247.5)
+                return "SW";
+            if (orientation <= 292.5)
+                return "W";
+            if (orientation <= 337.5)
+                return "NW";
+        }
+
+        if (orientation < -337.5 || orientation >= -22.5)
+            return "N";
+        if (orientation >= -67.5)
+            return "NW";
+        if (orientation >= -112.5)
+            return "W";
+        if (orientation >= -157.5)
+            return "SW";
+        if (orientation >= -202.5)
+            return "S";
+        if (orientation <= -247.5)
+            return "SE";
+        if (orientation <= -292.5)
+            return "E";
+        if (orientation <= -337.5)
+            return "NE";
+
+        return "ERROR";
+
+    }
+
 }
