8931a9046270d8772de9c623395a8c1f58e129ff
zfxd <zhang_weiyao@yahoo.com.sg>
2023-02-12 18:57:31 -0800

    InputActivity & data validation

--- app/src/main/java/com/example/myapplication/InputActivity.java
+++ app/src/main/java/com/example/myapplication/InputActivity.java
@@ -2,13 +2,86 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.text.NumberFormat;
 
 public class InputActivity extends AppCompatActivity {
+
+    private String label_parents;
+    private float lat_parents;
+    private float long_parents;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_input);
     }
+
+    public void onSubmitClicked(View view) {
+        // Data validation - maybe we want this in Utilities instead?
+        TextView label_parents_view = findViewById(R.id.label_parents);
+        TextView latlong_parents_view = findViewById(R.id.latlong_parents);
+
+        label_parents = label_parents_view.getText().toString();
+        String latlong_parents = latlong_parents_view.getText().toString();
+        if (label_parents.equals("")) {
+            Utilities.showAlert(this, "Please enter a label for your parents' house!");
+            return;
+        }
+
+        if (latlong_parents.equals("")) {
+            Utilities.showAlert(this, "Please enter your parents' house's coordinates!");
+            return;
+        }
+
+        String[] latlong_parents_split = latlong_parents.split(",");
+        if (latlong_parents_split.length != 2) {
+            // TODO: Currently "1,1,,,,,," (however many ','s) are accepted as valid coordinates
+            Utilities.showAlert(this, "Please enter coordinates as 'lat,long'");
+            return;
+        }
+
+        try {
+            lat_parents = Float.parseFloat(latlong_parents_split[0]);
+            long_parents = Float.parseFloat(latlong_parents_split[1]);
+        } catch (NumberFormatException e) {
+            Utilities.showAlert(this, "Please enter valid coordinates!");
+            return;
+        }
+
+        if (lat_parents < -90 || lat_parents > 90 || long_parents < -180 || long_parents > 180) {
+            // Might want less technical error messages in the final build
+            Utilities.showAlert(this, "Please ensure -90 < lat < 90 and -180 < long < 180");
+            return;
+        }
+
+        saveProfile();
+        finish();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        saveProfile();
+    }
+
+    private void saveProfile(){
+        SharedPreferences preferences = getSharedPreferences("saved_data", MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+
+
+        editor.putString("label_parents", this.label_parents);
+        editor.putFloat("lat_parents", this.lat_parents);
+        editor.putFloat("long_parents", this.long_parents);
+        Utilities.showAlert(this, label_parents);
+        Utilities.showAlert(this, Float.toString(lat_parents));
+        Utilities.showAlert(this, Float.toString(long_parents));
+        editor.apply();
+    }
+
+
 }
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.widget.TextView;
@@ -12,19 +14,31 @@ public class MainActivity extends AppCompatActivity {
     private OrientationService orientationService;
     private LocationService locationService;
 
+    private float lat_parents;
+    private float long_parents;
+    private String label_parents;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        loadProfile();
+        // If nothing saved, launch InputActivity
+        if (lat_parents == 91f || long_parents == 181f || label_parents == "")
+        {
+            Intent intent = new Intent(this, InputActivity.class);
+            startActivity(intent);
+        }
+
+        // Ask for location permissions
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
-            != PackageManager.PERMISSION_GRANTED &&
-            ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
-            != PackageManager.PERMISSION_GRANTED) {
+                != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                        != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
                     200);
         }
-
 
         orientationService = OrientationService.singleton(this);
         TextView orientation_text = findViewById(R.id.orientation_text);
@@ -45,5 +59,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onPause() {
         super.onPause();
         orientationService.unregisterSensorListeners();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // When returning from InputActivity, update the values so we can see
+        loadProfile();
+        TextView debug_parents = findViewById(R.id.debug_parents);
+        debug_parents.setText(label_parents + ": lat = " + lat_parents + ", long = " + long_parents);
+    }
+
+    private void loadProfile() {
+        SharedPreferences preferences = getSharedPreferences("saved_data", MODE_PRIVATE);
+
+        this.label_parents = preferences.getString("label_parents", "");
+        this.lat_parents = preferences.getFloat("lat_parents", 91f);
+        this.long_parents = preferences.getFloat("long_parents", 181f);
+
+        // TODO Do the same for the other 2 values...
     }
 }
