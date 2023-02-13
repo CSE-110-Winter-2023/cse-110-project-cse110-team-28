cec6e4e3b9ec68930d5fde09410f364efe538830
Stephen-Schuster <46509946+Stephen-Schuster@users.noreply.github.com>
2023-02-13 09:38:10 -0800

    Made second BDD test scenario
    The test is in Story2BDDTest.java

---
+++ app/src/test/java/com/example/myapplication/Story2BDDTest.java
@@ -0,0 +1,24 @@
+package com.example.myapplication;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class Story2BDDTest {
+    /**
+     * GIVEN My parent's house is at 40, -122
+     * AND I am at google HQ(37, -122)
+     * WHEN I load the main screen
+     * THEN my parent's house should be North of me
+     * WHEN I move 3 degrees east to 37, -119
+     * THEN my parent's house should change to Northwest of me
+     */
+    @Test
+    public void BDD2Test() {
+        float parentsLat = 40, parentsLong = -122;
+        float myLat = 37, myLong = -122;
+        assertEquals("N",Utilities.directionBetweenPoints(myLat,parentsLat,myLong,parentsLong));
+        myLong = -119;
+        assertEquals("NW",Utilities.directionBetweenPoints(myLat,parentsLat,myLong,parentsLong));
+    }
+}
