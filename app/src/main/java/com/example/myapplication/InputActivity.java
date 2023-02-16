f0a5d0abc0d1a037a94279dfc2708a3684e2667c
EShults2124 <shults.eric01@gmail.com>
2023-02-15 19:47:49 -0800

    Added Save for Set Orientation

--- app/src/main/java/com/example/myapplication/InputActivity.java
+++ app/src/main/java/com/example/myapplication/InputActivity.java
@@ -15,6 +15,7 @@ public class InputActivity extends AppCompatActivity {
     private String label_parents;
     private float lat_parents;
     private float long_parents;
+    private float set_orientation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +27,9 @@ public class InputActivity extends AppCompatActivity {
         // Data validation - maybe we want this in Utilities instead?
         TextView label_parents_view = findViewById(R.id.label_parents);
         TextView latlong_parents_view = findViewById(R.id.latlong_parents);
+        EditText set_orient_view = findViewById(R.id.set_orient);
 
+        String set_orient_string = set_orient_view.getText().toString();
 
         label_parents = label_parents_view.getText().toString();
         String latlong_parents = latlong_parents_view.getText().toString();
@@ -55,12 +58,26 @@ public class InputActivity extends AppCompatActivity {
             return;
         }
 
+        try {
+            set_orientation = Float.parseFloat(set_orient_string);
+        } catch (NumberFormatException e) {
+            Utilities.showAlert(this, "Please enter valid orientation!");
+            return;
+        }
+
         if (lat_parents < -90 || lat_parents > 90 || long_parents < -180 || long_parents > 180) {
             // Might want less technical error messages in the final build
             Utilities.showAlert(this, "Please ensure -90 < lat < 90 and -180 < long < 180");
             return;
         }
 
+        if (set_orientation < 0 || set_orientation >= 360) {
+            // Might want less technical error messages in the final build
+            Utilities.showAlert(this, "Please ensure -0 <= Orientation < 360");
+            return;
+        }
+        if (!set_orient_string.equals(""))
+            saveSetOrientation();
         saveProfile();
         finish();
     }
@@ -79,6 +96,15 @@ public class InputActivity extends AppCompatActivity {
         editor.putString("label_parents", this.label_parents);
         editor.putFloat("lat_parents", this.lat_parents);
         editor.putFloat("long_parents", this.long_parents);
+        editor.apply();
+    }
+
+    private void saveSetOrientation(){
+        SharedPreferences preferences = getSharedPreferences("saved_data", MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+
+        editor.putFloat("set_orientation", this.set_orientation);
+
         editor.apply();
     }
 
