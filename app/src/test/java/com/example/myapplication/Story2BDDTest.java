d64f268d8f3aba1b5d92cc2d607d504580f11961
zfxd <zhang_weiyao@yahoo.com.sg>
2023-02-19 21:21:47 -0800

    Fixed affected tests

--- app/src/test/java/com/example/myapplication/Story2BDDTest.java
+++ app/src/test/java/com/example/myapplication/Story2BDDTest.java
@@ -17,8 +17,8 @@ public class Story2BDDTest {
     public void BDD2Test() {
         float parentsLat = 40, parentsLong = -122;
         float myLat = 37, myLong = -122;
-        assertEquals("N", CoordinateUtil.directionBetweenPoints(myLat,parentsLat,myLong,parentsLong));
+        assertEquals("N", CoordinateUtil.cardDirection(CoordinateUtil.directionBetweenPoints(myLat,parentsLat,myLong,parentsLong)));
         myLong = -119;
-        assertEquals("NW", CoordinateUtil.directionBetweenPoints(myLat,parentsLat,myLong,parentsLong));
+        assertEquals("NW", CoordinateUtil.cardDirection(CoordinateUtil.directionBetweenPoints(myLat,parentsLat,myLong,parentsLong)));
     }
 }
