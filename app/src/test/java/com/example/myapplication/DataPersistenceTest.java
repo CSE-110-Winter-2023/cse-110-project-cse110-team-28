dcfb7abd34ca499233450133a7b9be05bda13a20
Kevin Son <43104401+CrazyApocalypticTerror@users.noreply.github.com>
2023-02-19 21:19:31 -0800

    Creating Test for user story 3

--- app/src/test/java/com/example/myapplication/DataPersistenceTest.java
+++ app/src/test/java/com/example/myapplication/DataPersistenceTest.java
@@ -1,2 +1,29 @@
-package com.example.myapplication;public class DataPersistenceTest {
+package com.example.myapplication;
+
+import static org.junit.Assert.assertEquals;
+
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.lifecycle.Lifecycle;
+import androidx.test.core.app.ActivityScenario;
+
+import com.example.myapplication.MainActivity;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+
+@RunWith(RobolectricTestRunner.class)
+public class DataPersistenceTest{
+    @Test
+    public void test_one_plus_one_equals_two()
+    {
+        var scenario = ActivityScenario.launch(MainActivity.class);
+        scenario.onActivity(activity -> {
+            EditText parentLabel = activity.findViewById(R.id.label_parents);
+
+
+        });
+    }
 }
