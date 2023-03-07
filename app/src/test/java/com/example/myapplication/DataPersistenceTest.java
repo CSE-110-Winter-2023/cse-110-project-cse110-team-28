a6548810446f28c474a3fd144d691bcd594076f9
Angelina Mkrtychyan <angelinamkr02@gmail.com>
2023-03-06 22:44:55 -0800

    compass picture

--- app/src/test/java/com/example/myapplication/DataPersistenceTest.java
+++ app/src/test/java/com/example/myapplication/DataPersistenceTest.java
@@ -1,2 +1,5 @@
 package com.example.myapplication;public class DataPersistenceTest {
 }
+//getting info from shared preferences after closing the app
+//instrumental test
+//location service and orientation service tests
